01fcee84a885351ee69175ebbb32c2d4318359df
Paul Park <paulpark673@gmail.com>
2020-09-15 00:03:08 -0700

    updated categories branch and updated ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Navbar from "./components/Layout/Navbar";
 import ProfileTab from "./components/ProfileTab/ProfileTab";
-import CategoryList from "./components/Categories/CategoryList/CategoryList";
+import ApplicationList from "./components/Applications/ApplicationList/ApplicationList";
 import "./App.css";
 
 function App() {
+  const applicationStages = ["applied", "in process", "offers"];
   return (
     <div>
       <Navbar />
       <main className="main-container">
         <ProfileTab />
-        <div className="categories-container">
-          <CategoryList />
-          <CategoryList />
-          <CategoryList />
+        <div className="applications-container">
+          {applicationStages.map((stage) => (
+            <ApplicationList stage={stage} />
+          ))}
         </div>
       </main>
     </div>
--- client/src/components/Categories/CategoryList/CategoryList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CategoryList = (props) => {
-  return <h1>Category List branch</h1>;
-};
-
-export default CategoryList;
