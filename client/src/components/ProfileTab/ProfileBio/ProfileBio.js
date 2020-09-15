b6b858e0d845dd2ade44794a05cce6588b14eed2
Paul Park <paulpark673@gmail.com>
2020-09-14 21:13:02 -0700

    testing profile branch

--- client/src/components/ProfileTab/ProfileBio/ProfileBio.js
+++ client/src/components/ProfileTab/ProfileBio/ProfileBio.js
@@ -4,7 +4,7 @@ import "./ProfileBio.css";
 const ProfileBio = (props) => {
   return (
     <div className="profile__bio">
-      <h1>Paul Park</h1>
+      <h1>Brian Hong</h1>
       <p>Front End Engineer</p>
       <p>Google</p>
       <p>paulpark.com</p>
