6938a134e81e88721aa76e83ade5f7ff641678f7
Paul Park <paulpark673@gmail.com>
2020-09-15 23:09:47 -0700

    added profile photo and removed box shadows

--- client/src/components/ProfileTab/ProfileBio/ProfileBio.js
+++ client/src/components/ProfileTab/ProfileBio/ProfileBio.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./ProfileBio.css";
+import profpic from "../../../assets/profpic.jpeg";
 
 const ProfileBio = (props) => {
   return (
     <div className="profile__bio">
-      <h1>Brian Hong</h1>
-      <p>Front End Engineer</p>
-      <p>Google</p>
-      <p>paulpark.com</p>
-      <p>Los Angeles, CA</p>
+      <div className="prof profile__pic">
+        <img className="img" alt="me" src={profpic} />
+      </div>
+      <div className="prof profile__name">Paul Park</div>
+      <div className="prof profile__title">Front End Engineer</div>
+      <div className="prof profile__job">Google</div>
+      <div className="prof profile__email">paulpark673@gmail.com</div>
+      <div className="prof profile__location">Los Angeles, CA</div>
     </div>
   );
 };
